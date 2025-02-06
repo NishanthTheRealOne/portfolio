971110032c73276178c24da7c5f81875f0598042
Srinivassane Nishanth <nishanth.srinivassane@etu.univ-lyon1.fr>
2025-02-06 16:28:13 +0100

    modification of the src attribute in the img's footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,13 @@ const Footer = () => {
             {/* the "target=_blank" attribute make the link to open in a new tab */}
             <div className={styles.contact} id="contact">
                 <a href="https://github.com/NishanthTheRealOne" className={styles.socialLink} target="_blank" rel="noopener noreferrer">
-                    <img src="/images/github-mark.png" alt="github icon" className={styles.socialIcon} />
+                    <img src={require("../images/github-mark.png")} alt="github icon" className={styles.socialIcon} />
                 </a>
                 <a href="mailto:nsriniv37@gmail.com" className={styles.socialLink} target="_blank" rel="noopener noreferrer">
-                    <img src="/images/icons8-gmail-48.png" alt="gmail icon" className={styles.socialIcon} />
+                    <img src={require("../images/icons8-gmail-48.png")} alt="gmail icon" className={styles.socialIcon} />
                 </a>
                 <a href="https://www.linkedin.com/in/nishanth-srinivassane-4083a6297/" className={styles.socialLink} target="_blank" rel="noopener noreferrer">
-                    <img src="/images/icons8-linkedin-48.png" alt="linkedin icon" className={styles.socialIcon} />
+                    <img src={require("../images/icons8-linkedin-48.png")} alt="linkedin icon" className={styles.socialIcon} />
                 </a>
             </div>
             <div className={styles.credits}>
