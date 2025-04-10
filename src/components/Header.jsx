3d9a8e33d3e8d3e0fb55b30f867ca21707ba789c
Srinivassane Nishanth <nishanth.srinivassane@etu.univ-lyon1.fr>
2025-04-10 20:19:00 +0200

    update of the header with a logo and a dawn img, add of an explanation for the dawnImg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,8 +11,12 @@ const Header = () => {
         });
     };
 
+
     return (
         <header className={styles.header}>
+            <div>
+                <img src={require("../images/logo.png")} alt='Logo' className={styles.logoImage} />
+            </div>
             <nav className={styles.nav}>
                 <div>
                     <a href='#introduction' className={styles.navLink} onClick={handleClick}>Introduction</a>
@@ -20,6 +24,15 @@ const Header = () => {
                     <a href='#contact' className={styles.navLink} onClick={handleClick}>Contact</a>
                 </div>
             </nav>
+            <div className={styles.dawnContainer}>
+                <img 
+                    src={require("../images/dawn.png")} 
+                    alt="Aube" 
+                    className={styles.dawn}
+                />
+                <span className={styles.explainImg}>Nishanth signifie "très tôt le matin" en Tamil</span>
+            </div>    
+            
         </header>
     );
 };
